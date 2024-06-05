78bb88cf1197ec288c3a3b0768740d3a85373eba
Dalia Tent <daliatent09@gmail.com>
2024-06-06 00:22:09 +0300

    Add project files.

---
+++ tema7/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Un autoturism detine un motor precum si un numar de usi. De asemenea autovehiculul este caracterizat de marca, model si an de fabricatie
+• Motorul
+ este caracterizat de
+ Capacitate cilindrica
+ Putere
+ Tipul combustibilului
+• Autoturismul va avea urmatoarele metode/proprietati
+ GetDescription
+ Va returna descrierea completa a autovehicolului sub forma unui sir de caractere
+ “Marca, model, anDeFabricatie, capacitate cilindrica, combustibil, putere motor, numar de usi.
+In metoda Main, creati autoturisme si afisati-le descrierea*/
